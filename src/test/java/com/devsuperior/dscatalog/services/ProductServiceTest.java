a73ccfb05798cba1a31f2eaa3f332c57e22b3910
Jonatas <jonatasmateuscontato@gmail.com>
2025-01-28 20:27:15 -0300

    Teste DatabaseException com ID dependente

--- src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
+++ src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,14 +25,19 @@ class ProductServiceTest {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 4L;
+
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 
         Mockito.when(repository.existsById(existingId)).thenReturn(true);
         Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
+        Mockito.when(repository.existsById(dependentId)).thenReturn(true);
     }
 
     @Test
@@ -46,6 +53,13 @@ class ProductServiceTest {
     void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
         assertThrows(ResourceNotFoundException.class, () -> {
             service.delete(nonExistingId);
+        });
+    }
+
+    @Test
+    void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        assertThrows(DatabaseException.class, () -> {
+            service.delete(dependentId);
         });
     }
 }
