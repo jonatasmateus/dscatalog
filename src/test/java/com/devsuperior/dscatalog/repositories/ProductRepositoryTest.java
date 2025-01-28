9603d413c130ef4f21667de440081d4f9e9ec495
Jonatas <jonatasmateuscontato@gmail.com>
2025-01-28 16:11:43 -0300

    Teste save com id null

--- src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
+++ src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.repositories;
 
+import com.devsuperior.dscatalog.Factory;
 import com.devsuperior.dscatalog.entities.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,10 +18,12 @@ class ProductRepositoryTest {
     private ProductRepository repository;
 
     private long existingId;
+    private long countTotalProducts;
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
+        countTotalProducts = 25L;
     }
 
     @Test
@@ -32,4 +35,20 @@ class ProductRepositoryTest {
         Optional<Product> result = repository.findById(existingId);
         assertFalse(result.isPresent());
     }
+
+    @Test
+    void saveShouldPersistWithAutoincrementWhenIsNull() {
+        // Arrange
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        // Act
+        Product result = repository.save(product);
+
+        // Assert
+        assertNotNull(result.getId());
+        assertEquals(countTotalProducts + 1, product.getId());
+    }
+
+
 }
